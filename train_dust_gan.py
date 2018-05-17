d16f15da52bc623359ad73c35d59a2e79f7765fa
kmaylor <kmaylor@ucdavis.edu>
2018-05-17 12:38:11 -0700

    fixed normalization of training data

--- train_dust_gan.py
+++ train_dust_gan.py
@@ -1,8 +1,8 @@
 from dust_gan import DustDCGAN
 print('Instantiate GAN')
-dust_gan = DustDCGAN('/global/homes/k/kmaylor/cori/Maps_and_Makers/Planck_dust_cuts_353GHz.pk',load_state=True)
+dust_gan = DustDCGAN('D:\Projects\Maps_and_Makers\Planck_dust_cuts_353GHz.pk',load_state=False)
 print('Begin Training')
-dust_gan.train(train_steps=2000, batch_size=32, save_interval=500)
+dust_gan.train(train_steps=2000, batch_size=8, save_interval=100)
 print('Saving GAN')
 dust_gan.DCGAN.save_dcgan()
 #dust_gan.plot_images(fake=True)
